05666f8a3cdca73d051f3a3e1d15ad42e2ce73b0
GustavoA198 <adolfogutierreza0108@gmail.com>
2025-03-20 23:14:10 -0500

    Feat: Configuración inicial
    Configuración inicial del proyecto. Creada con spring initializr

---
+++ backend/CinePass-app/src/main/java/com/cinePass/CinePass_app/CinePassAppApplication.java
@@ -0,0 +1,13 @@
+package com.cinePass.CinePass_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinePassAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinePassAppApplication.class, args);
+	}
+
+}
